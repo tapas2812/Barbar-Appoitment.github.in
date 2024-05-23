d02697fefbd3d0509c9db2534a3df4370be43010
tapas2812 <98145552+tapas2812@users.noreply.github.com>
2024-05-23 20:51:53 +0530

    Barbar-Appoitment

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// Menu Bar******************************************************
+var menubtn = document.getElementById("menubtn");
+var nav = document.getElementById("nav");
+var menu = document.getElementById("menu");
+
+
+nav.style.right = "-200px";
+
+menubtn.onclick = function(){
+    if(nav.style.right == "-200px")
+        {
+        nav.style.right = "0";
+        menu.src = "images/close.png";
+    }
+    else{
+        nav.style.right = "-200px";
+        menu.src = "images/menu.png";
+    }
+}
+        
